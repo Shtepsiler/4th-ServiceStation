6fbbc0558e06f299bcb3a966aabc165c6e8e73ea
Shtepsiler <horyniuk.stepan@chnu.edu.ua>
2024-11-10 00:46:36 +0200

    Add project files.

---
+++ JOBS.BLL/DTOs/Respponces/SpecialisationDTO.cs
@@ -0,0 +1,18 @@
+﻿using JOBS.BLL.Common.Mappings;
+using JOBS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOBS.BLL.DTOs.Respponces
+{
+    public  class SpecialisationDTO : IMapFrom<Specialisation>
+    {
+        public string Name { get; set; }
+
+        
+
+    }
+}
